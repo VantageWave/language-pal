c73b30acc71c1382089b4aef5b0734a50f29501c
Rafal Grabianski <rafal.grm@gmail.com>
2023-06-05 19:08:47 +0200

    Merge local changes for hackathon submission

--- cloud_funcs/main.py
+++ cloud_funcs/main.py
@@ -1,10 +1,10 @@
 import functions_framework
-from v4.text_analysis import summarize_text
+from v4.text_analysis import summarize_text, ask_question_text
 
 @functions_framework.http
 def v4_summarizeText(request):
     return summarize_text(request)
     
-# @functions_framework.http
-# def v4_askQuestionText(request):
-#     return summarize_text(request)
+@functions_framework.http
+def v4_askQuestionText(request):
+    return ask_question_text(request)
